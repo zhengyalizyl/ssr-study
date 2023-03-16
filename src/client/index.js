d72c8045eb391e2a31f92fb28b93d375db3cfe8f
lizheng <v-lizheng@expediagroup.com>
2023-03-16 21:32:01 +0800

    add router

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../containers/App';
+import { BrowserRouter } from 'react-router-dom'
+import { routerRoutes } from 'react-router-config';
 
-ReactDOM.hydrate(<App/> , document.getElementById('root'));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById('root'));
